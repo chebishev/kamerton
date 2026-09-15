581758f2cd4c0efba9cdf696c06068d6976533c9
chebishev <atanas.chebishev@gmail.com>
2026-09-15 11:15:32 +0300

    add email info add new images for round devices

--- page/gt/home/about.r.layout.js
+++ page/gt/home/about.r.layout.js
@@ -19,23 +19,23 @@ export const CREATOR_INFO = {
     x: px(96),
     y: px(130),
     w: px(288),
-    h: px(46),
+    h: px(48),
     color: 0xffffff,
     text_size: px(19),
     align_h: align.CENTER_H,
     align_v: align.CENTER_V,
     text_style: text_style.WRAP,
-    text: getText("creator")
+    text: `${getText("creator")}\n${getText("email")}`
 };
 
 export const QRCODE = {
       content: 'https://chebishev.github.io/',
       x: px(140),
-      y: px(200),
+      y: px(210),
       w: px(200),
       h: px(200),
       bg_x: px(120),
-      bg_y: px(180),
+      bg_y: px(190),
       bg_w: px(240),
       bg_h: px(240)
 }
--- page/gt/home/index.r.layout.js
+++ page/gt/home/index.r.layout.js
@@ -2,17 +2,17 @@ import { getDeviceInfo } from "@zos/device";
 import { px } from "@zos/utils";
 
 export const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = getDeviceInfo();
-const infoPosition = DEVICE_WIDTH / 2 - 21
+const infoPosition = DEVICE_WIDTH / 2 - 20
 
 export const BACKGROUND_PRESSED = "pressed.png"
 export const BACKGROUND = {
   src: "static.png",
-  x: px(-21),
-  y: px(-21),
+  x: px(-24),
+  y: px(-22),
 };
 
 export const INFO_ICON = {
   src: "info.png",
   x: px(infoPosition),
-  y: px(417),
+  y: px(6),
 }
